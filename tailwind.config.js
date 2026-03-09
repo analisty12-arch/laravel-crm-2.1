c160b2c3f1c5a7e9c40fb6886b6526efa2d7d7e5
lanhouseanalisty-afk <lanhouseanalisty@gmail.com>
2026-03-09 19:27:16 -0300

    fix: v2.1 deployment - correct types and esm config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -173,5 +175,5 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animate")],
+    plugins: [tailwindAnimate],
 }
